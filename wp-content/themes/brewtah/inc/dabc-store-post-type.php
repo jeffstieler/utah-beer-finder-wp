0280e0af53e8b226976a985881f6091d2fbab7d0
Jeff Stieler <jeff.m.stieler@gmail.com>
2014-10-04 22:48:53 -0600

    Copy pasta, badbadbadbadbad.

--- wp-content/themes/brewtah/inc/dabc-store-post-type.php
+++ wp-content/themes/brewtah/inc/dabc-store-post-type.php
@@ -17,6 +17,7 @@ class DABC_Store_Post_Type {
 	const PHONE_NUMBER    = 'dabc-store-phone';
 	const LATITUDE        = 'dabc-store-latitude';
 	const LONGITUDE       = 'dabc-store-longitude';
+	const STORES_JS_URL   = 'http://abc.utah.gov/common/script/abcMap.js';
 
 	var $titan;
 
@@ -118,6 +119,31 @@ class DABC_Store_Post_Type {
 		register_taxonomy( self::CITY_TAXONOMY, self::POST_TYPE, array(
 			'label' => 'City'
 		) );
+
+	}
+
+	/**
+	 * HTTP request helper
+	 *
+	 * @param string $url URL to request
+	 * @param array $args wp_remote_request() arguments
+	 * @return boolean|WP_Error|string - bool on non-200, WP_Error on error, string body on 200
+	 */
+	function _make_http_request( $url, $args = array() ) {
+
+		$response = wp_remote_request( $url, $args );
+
+		if ( is_wp_error( $response ) ) {
+
+			return $response;
+
+		} else if ( 200 === wp_remote_retrieve_response_code( $response ) ) {
+
+			return wp_remote_retrieve_body( $response );
+
+		}
+
+		return false;
 
 	}
 
