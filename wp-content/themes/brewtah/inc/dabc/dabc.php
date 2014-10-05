5773f3f1b55c6f246673345eb9a8c83dc127089e
Jeff Stieler <jeff.m.stieler@gmail.com>
2014-10-05 14:48:13 -0600

    Add O2O connections file, map out Store->Beers connection.

--- wp-content/themes/brewtah/inc/dabc/dabc.php
+++ wp-content/themes/brewtah/inc/dabc/dabc.php
@@ -9,3 +9,9 @@ require_once( 'dabc-beer-post-type.php' );
  * Include the DABC store post type
  */
 require_once( 'dabc-store-post-type.php' );
+
+/**
+ * Register connections between post types
+ */
+require_once( 'o2o-connections.php' );
+
---
+++ wp-content/themes/brewtah/inc/dabc/o2o-connections.php
@@ -0,0 +1,37 @@
+<?php
+
+class DABC_O2O_Connections {
+	
+	const DABC_STORE_BEERS = 'dabc_store_beers';
+	
+	function init() {
+		
+		O2O::Register_Connection(
+			self::DABC_STORE_BEERS,
+			DABC_Store_Post_Type::POST_TYPE,
+			DABC_Beer_Post_Type::POST_TYPE,
+			array(
+				'reciprocal'   => true,
+				'hierarchical' => false,
+				'to'           => array(
+					'sortable' => false,
+					'labels'   => array(
+						'name'          => 'Beers',
+						'singular_name' => 'Beer'
+					)
+				),
+				'from'         => array(
+					'sortable' => false,
+					'labels'   => array(
+						'name'          => 'Stores',
+						'singular_name' => 'Store'
+					)
+				)
+			)
+		);
+		
+	}
+	
+}
+
+add_action( 'init', array( new DABC_O2O_Connections(), 'init' ) );
