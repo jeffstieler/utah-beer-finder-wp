2d0a5b3b439032e35aff521646d39c596d3e5342
Jeff Stieler <jeff.m.stieler@gmail.com>
2014-10-05 18:54:11 -0600

    Add wrapper to call a given O2O connection’s set_connected_to() method.

--- wp-content/themes/brewtah/inc/dabc/o2o-connections.php
+++ wp-content/themes/brewtah/inc/dabc/o2o-connections.php
@@ -1,12 +1,23 @@
 <?php
 
 class DABC_O2O_Connections {
-	
+
 	const DABC_STORE_BEERS = 'dabc_store_beers';
-	
+
+	var $o2o;
+	var $o2o_connection_factory;
+
+	function __construct() {
+
+		$this->o2o = O2O::GetInstance();
+
+		$this->o2o_connection_factory = $this->o2o->get_connection_factory();
+
+	}
+
 	function init() {
-		
-		O2O::Register_Connection(
+
+		$this->o2o->Register_Connection(
 			self::DABC_STORE_BEERS,
 			DABC_Store_Post_Type::POST_TYPE,
 			DABC_Beer_Post_Type::POST_TYPE,
@@ -29,7 +40,30 @@ class DABC_O2O_Connections {
 				)
 			)
 		);
-		
+
 	}
-	
+
+	/**
+	 * Helper to call a given O2O connetion's set_connected_to() method
+	 *
+	 * @param string $connection_name
+	 * @param int $from_object_id
+	 * @param array|int $connected_to_ids
+	 * @param bool $append
+	 * @return bool|WP_Error true on success, WP_Error on connection not found or other error
+	 */
+	function set_connected_to( $connection_name, $from_object_id, $connected_to_ids = array(), $append = false ) {
+
+		$connection = $this->o2o_connection_factory->get_connection( $connection_name );
+
+		if ( false === $connection ) {
+
+			return new WP_Error( 'invalid_connection_name', 'The given connection name is not valid.' );
+
+		}
+
+		return $connection->set_connected_to( $from_object_id, $connected_to_ids, $append );
+
+	}
+
 }
