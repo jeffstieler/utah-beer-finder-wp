b9b0fc5bc77a37c8618fafe852a6f1d1a2cacf1d
Jeff Stieler <jeff.m.stieler@gmail.com>
2014-10-16 18:53:23 -0600

    Fix linking thumbnail and title for non-single beers in info part.

--- wp-content/themes/brewtah/parts/beer-info.php
+++ wp-content/themes/brewtah/parts/beer-info.php
@@ -1,11 +1,11 @@
 <div class="row beer-info">
 	<div class="small-12 large-3 columns">
 		<?php $thumbnail_size = is_single() ? 'beer-single-image' : 'thumbnail'; ?>
-		<?php if ( is_search() ) : ?>
+		<?php if ( ! is_single() ) : ?>
 		<a href="<?php the_permalink(); ?>">
 		<?php endif; ?>
 		<?php the_post_thumbnail( $thumbnail_size, array( 'class' => 'th' ) ); ?>
-		<?php if ( is_search() ) : ?>
+		<?php if ( ! is_single() ) : ?>
 		</a>
 		<?php endif; ?>
 	</div>
@@ -15,9 +15,9 @@
 				<?php if ( is_single() ) : ?>
 				<h1 class="entry-title"><?php the_title(); ?></h1>
 				<?php else : ?>
-				<a href="<?php the_permalink(); ?>">
-					<h3 class="entry-title"><?php the_title(); ?></h3>
-				</a>
+				<h3 class="entry-title">
+					<a href="<?php the_permalink(); ?>"><?php the_title(); ?></a>
+				</h3>
 				<?php endif; ?>
 			</div>
 		</div>
