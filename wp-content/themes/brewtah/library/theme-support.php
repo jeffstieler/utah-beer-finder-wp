28922e46d3820f7d59ca8f442ec5cba1e0b7834e
Jeff Stieler <jeff.m.stieler@gmail.com>
2014-10-16 18:22:46 -0600

    Add image sizes.

--- wp-content/themes/brewtah/library/theme-support.php
+++ wp-content/themes/brewtah/library/theme-support.php
@@ -8,7 +8,8 @@ function brewtah_theme_support() {
 
     // Add post thumbnail support: http://codex.wordpress.org/Post_Thumbnails
     add_theme_support('post-thumbnails');
-    // set_post_thumbnail_size(150, 150, false);
+
+	set_post_thumbnail_size(150, 150, false);
 
     // rss thingy
     add_theme_support('automatic-feed-links');
@@ -16,7 +17,12 @@ function brewtah_theme_support() {
     // Add post formarts support: http://codex.wordpress.org/Post_Formats
     add_theme_support('post-formats', array('aside', 'gallery', 'link', 'image', 'quote', 'status', 'video', 'audio', 'chat'));
 
+	// Add image sizes
+	add_image_size( 'beer-single-image', 250, 250, true );
+
+	add_image_size( 'beer-sidebar-image', 50, 50, true );
+
 }
 
-add_action('after_setup_theme', 'brewtah_theme_support'); 
+add_action('after_setup_theme', 'brewtah_theme_support');
 ?>
