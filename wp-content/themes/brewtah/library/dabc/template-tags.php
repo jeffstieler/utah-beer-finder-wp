1593b3d984d66761df79ea5aa48f348da72b0c64
Jeff Stieler <jeff.m.stieler@gmail.com>
2014-10-19 00:20:28 -0600

    Allow omission of post ID if in loop for template tags.

--- wp-content/themes/brewtah/library/dabc/template-tags.php
+++ wp-content/themes/brewtah/library/dabc/template-tags.php
@@ -72,7 +72,9 @@ function dabc_query_stores_by_number( $store_number ) {
 
 }
 
-function dabc_get_store_number( $post_id ) {
+function dabc_get_store_number( $post_id = null ) {
+
+	$post_id = $post_id ?: get_the_ID();
 
 	$dabc = new DABC();
 
@@ -80,7 +82,9 @@ function dabc_get_store_number( $post_id ) {
 
 }
 
-function dabc_get_store_address( $post_id ) {
+function dabc_get_store_address( $post_id = null ) {
+
+	$post_id = $post_id ?: get_the_ID();
 
 	$dabc = new DABC();
 
@@ -88,13 +92,15 @@ function dabc_get_store_address( $post_id ) {
 
 }
 
-function dabc_the_store_address( $post_id ) {
+function dabc_the_store_address( $post_id = null ) {
 
 	echo dabc_get_store_address( $post_id );
 
 }
 
-function dabc_get_store_phone_number( $post_id ) {
+function dabc_get_store_phone_number( $post_id = null ) {
+
+	$post_id = $post_id ?: get_the_ID();
 
 	$dabc = new DABC();
 
@@ -102,13 +108,15 @@ function dabc_get_store_phone_number( $post_id ) {
 
 }
 
-function dabc_the_store_phone_number( $post_id ) {
+function dabc_the_store_phone_number( $post_id = null ) {
 
 	echo dabc_get_store_phone_number( $post_id );
 
 }
 
-function dabc_get_store_tel_link( $post_id ) {
+function dabc_get_store_tel_link( $post_id = null ) {
+
+	$post_id = $post_id ?: get_the_ID();
 
 	$dabc = new DABC();
 
@@ -116,7 +124,7 @@ function dabc_get_store_tel_link( $post_id ) {
 
 }
 
-function dabc_the_store_tel_link( $post_id ) {
+function dabc_the_store_tel_link( $post_id = null ) {
 
 	echo dabc_get_store_tel_link( $post_id );
 
