a0a0578d46b6cdba0ae21fab0fc3c4f1326f3060
Jeff Stieler <jeff.m.stieler@gmail.com>
2014-10-21 20:29:53 -0600

    Refactor beer availability to use template tags.

--- wp-content/themes/brewtah/parts/beer-availability.php
+++ wp-content/themes/brewtah/parts/beer-availability.php
@@ -1,14 +1,15 @@
 <section>
 <?php
-$inventory = get_post_meta(get_the_ID(), 'dabc-store-inventory', true);
 
-if ( $inventory['inventory'] ) :
+$beer_post_id = get_the_ID();
 
-	$store_numbers = array_keys( $inventory['inventory'] );
+if ( $inventory = dabc_get_inventory( $beer_post_id ) ) :
+
+	$store_numbers = array_keys( $inventory );
 
 ?>
 	<h3>Store Availability</h3>
-	<p>Last Updated: <?php echo $inventory['last_updated']; ?></p>
+	<p>Last Updated: <?php dabc_the_inventory_last_updated(); ?></p>
 	<table>
 		<thead>
 			<tr>
@@ -30,7 +31,7 @@ if ( $inventory['inventory'] ) :
 				<td>
 					<?php dabc_the_store_address( $store_post_id ); ?>
 				</td>
-				<td><?php echo $inventory['inventory'][$store_number]; ?></td>
+				<td><?php dabc_the_quantity_for_store( $store_number, $beer_post_id ); ?></td>
 			</tr>
 			<?php endwhile; ?>
 		</tbody>
