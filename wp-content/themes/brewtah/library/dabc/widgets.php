7ab70db7b910c61ab0010cac56cf33ab8d133e02
Jeff Stieler <jeff.m.stieler@gmail.com>
2014-10-26 17:20:40 -0600

    Add beer search tools widget, just search for now.

--- wp-content/themes/brewtah/library/dabc/widgets.php
+++ wp-content/themes/brewtah/library/dabc/widgets.php
@@ -202,3 +202,84 @@ class DABC_New_Beers_Widget extends DABC_Top_Rated_Beers_Widget {
 }
 
 register_widget( 'DABC_New_Beers_Widget' );
+
+/**
+ * Beer search tools widget
+ */
+class DABC_Beer_Search_Tools extends WP_Widget {
+
+	var $cache_key;
+
+	public function __construct() {
+
+		$this->cache_key = 'widget_beer_search_tools';
+
+		$widget_ops = array(
+			'classname'   => 'widget_beer_search_tools',
+			'description' => 'Tools to find beers'
+		);
+
+		parent::__construct( 'beer-search-tools', 'Beer Search Tools', $widget_ops );
+
+		add_action( 'save_post', array( $this, 'flush_widget_cache' ) );
+		add_action( 'deleted_post', array( $this, 'flush_widget_cache' ) );
+
+	}
+
+	public function widget( $args, $instance ) {
+
+		/** This filter is documented in wp-includes/default-widgets.php */
+		$title = apply_filters( 'widget_title', empty( $instance['title'] ) ? '' : $instance['title'], $instance, $this->id_base );
+
+		echo $args['before_widget'];
+		if ( $title ) {
+			echo $args['before_title'] . $title . $args['after_title'];
+		}
+
+		// Use current theme search form if it exists
+		get_search_form();
+
+		echo $args['after_widget'];
+
+	}
+
+	public function form( $instance ) {
+
+		$instance = wp_parse_args(
+			(array) $instance,
+			array(
+				'title' => ''
+			)
+		);
+
+		$title = $instance['title'];
+		?>
+		<p>
+			<label for="<?php echo $this->get_field_id( 'title' ); ?>"><?php _e( 'Title:' ); ?></label>
+			<input class="widefat" id="<?php echo $this->get_field_id( 'title' ); ?>" name="<?php echo $this->get_field_name( 'title' ); ?>" type="text" value="<?php echo $title; ?>" />
+		</p>
+		<?php
+
+	}
+
+	public function update( $new_instance, $old_instance ) {
+
+		$instance = $old_instance;
+
+		$instance['title'] = strip_tags( $new_instance['title'] );
+
+		$this->flush_widget_cache();
+
+		return $instance;
+
+	}
+
+	public function flush_widget_cache() {
+
+		wp_cache_delete( $this->cache_key, 'widget' );
+
+	}
+
+}
+
+register_widget( 'DABC_Beer_Search_Tools' );
