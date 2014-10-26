3822cd04281d3bbb670dc4d7fab1024cc7f71bef
Jeff Stieler <jeff.m.stieler@gmail.com>
2014-10-26 12:43:05 -0600

    Remove “download” button from homepage.

--- wp-content/themes/brewtah/front-page.php
+++ wp-content/themes/brewtah/front-page.php
@@ -5,7 +5,6 @@
 		<div class="small-12 medium-4 columns">
 			<h1><a href="<?php bloginfo('url'); ?>" title="<?php bloginfo('name'); ?>"><?php bloginfo('name'); ?></a></h1>
 			<h4 class="subheader"><?php bloginfo('description'); ?></h4>
-			<a role="button" class="download large button hide-for-small" href="https://github.com/olefredrik/brewtah">Download brewtah</a>
 		</div>
 
 		<div class="medium-8 columns show-for-medium-up">
