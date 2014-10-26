7002a641520b51e884daeaefaa76b03da01037d4
Jeff Stieler <jeff.m.stieler@gmail.com>
2014-10-25 23:47:11 -0600

    Rework homepage to use hero section with map, and sidebar for widgets below.

--- wp-content/themes/brewtah/front-page.php
+++ wp-content/themes/brewtah/front-page.php
@@ -1,12 +1,23 @@
 <?php get_header(); ?>
-<div class="row">
-	<div class="small-12 large-8 columns" role="main">
 
-		<div class="flex-video">
-			<div id="store-map" style="width: 100%; height: 100%; min-width: 200px; min-height: 400px;"></div>
+<header id="homepage-hero" role="banner">
+	<div class="row">
+		<div class="small-12 medium-4 columns">
+			<h1><a href="<?php bloginfo('url'); ?>" title="<?php bloginfo('name'); ?>"><?php bloginfo('name'); ?></a></h1>
+			<h4 class="subheader"><?php bloginfo('description'); ?></h4>
+			<a role="button" class="download large button hide-for-small" href="https://github.com/olefredrik/brewtah">Download brewtah</a>
 		</div>
-		
+
+		<div class="medium-8 columns show-for-medium-up">
+			<div id="store-map"></div>
+		</div>
+
 	</div>
-	<?php get_sidebar(); ?>
+
+</header>
+
+<div class="row">
+	<?php dynamic_sidebar( 'homepage-widgets' ); ?>
 </div>
+
 <?php get_footer(); ?>
