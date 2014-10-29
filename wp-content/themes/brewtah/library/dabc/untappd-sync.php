8e03cf3e59660fae2ce50fe61b2a9341e6bb6426
Jeff Stieler <jeff.m.stieler@gmail.com>
2014-10-28 23:18:56 -0600

    First pass at Untappd beer search method.

--- wp-content/themes/brewtah/library/dabc/untappd-sync.php
+++ wp-content/themes/brewtah/library/dabc/untappd-sync.php
@@ -47,4 +47,59 @@ class Untappd_Sync {
 
 	}
 
+	/**
+	 * HTTP request helper
+	 *
+	 * @param string $url URL to request
+	 * @param array $args wp_remote_request() arguments
+	 * @return boolean|WP_Error|string - bool on non-200, WP_Error on error, string body on 200
+	 */
+	function _make_http_request( $url, $args = array() ) {
+
+		$response = wp_remote_request( $url, $args );
+
+		if ( is_wp_error( $response ) ) {
+
+			return $response;
+
+		} else if ( 200 === wp_remote_retrieve_response_code( $response ) ) {
+
+			return wp_remote_retrieve_body( $response );
+
+		}
+
+		return false;
+
+	}
+
+	/**
+	 * Search for beers on Untappd
+	 *
+	 * @param string $query
+	 * @return bool|WP_Error|string
+	 */
+	function search( $query ) {
+
+		if ( ! defined( 'UNTAPPD_CLIENT_ID' ) || ! defined( 'UNTAPPD_CLIENT_SECRET' ) ) {
+
+			return false;
+
+		}
+
+		$url = add_query_arg(
+			array(
+				'q'             => urlencode( $query ),
+				'sort'          => 'count',
+				'client_id'     => UNTAPPD_CLIENT_ID,
+				'client_secret' => UNTAPPD_CLIENT_SECRET
+			),
+			'https://api.untappd.com/v4/search/beer'
+		);
+
+		$response = $this->_make_http_request( $url );
+
+		return $response;
+
+	}
+
 }
