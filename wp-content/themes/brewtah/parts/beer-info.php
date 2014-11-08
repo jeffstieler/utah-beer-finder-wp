f840ad2a5f505a9550c435ad6d56b6ed65e6cd3f
Jeff Stieler <jeff.m.stieler@gmail.com>
2014-11-07 22:40:24 -0700

    Show Untappd rating in beer info, remove calories, use new beer size, show brewery and style.

--- wp-content/themes/brewtah/parts/beer-info.php
+++ wp-content/themes/brewtah/parts/beer-info.php
@@ -1,42 +1,43 @@
 <div class="row beer-info">
-	<div class="small-12 large-3 columns">
-		<?php $thumbnail_size = is_single() ? 'beer-single-image' : 'thumbnail'; ?>
+	<div class="small-12 large-2 columns">
 		<?php if ( ! is_single() ) : ?>
 		<a href="<?php the_permalink(); ?>">
 		<?php endif; ?>
-		<?php the_post_thumbnail( $thumbnail_size, array( 'class' => 'th' ) ); ?>
+		<?php the_post_thumbnail( 'beer-image', array( 'class' => 'th' ) ); ?>
 		<?php if ( ! is_single() ) : ?>
 		</a>
 		<?php endif; ?>
 	</div>
-	<div class="small-12 large-9 columns">
+	<div class="small-12 large-10 columns">
 		<div class="row">
 			<div class="large-12 columns">
 				<?php if ( is_single() ) : ?>
 				<h1 class="entry-title"><?php the_title(); ?></h1>
 				<?php else : ?>
-				<h3 class="entry-title">
+				<h5 class="entry-title">
 					<a href="<?php the_permalink(); ?>"><?php the_title(); ?></a>
-				</h3>
+				</h5>
 				<?php endif; ?>
+				<h6><?php the_terms( get_the_ID(), 'beer-brewery' ); ?></h6>
+				<h6><?php the_terms( get_the_ID(), 'beer-style' ); ?></h6>
 			</div>
 		</div>
 		<div class="row">
 			<div class="large-3 small-3 columns">
-				<span><?php dabc_the_overall_rating(); ?></span>
-				<p>Overall Score</p>
+				<span><?php dabc_the_overall_rating(); ?> / 100</span>
+				<p>RB Overall</p>
 			</div>
 			<div class="large-3 small-3 columns">
-				<span><?php dabc_the_style_rating(); ?></span>
-				<p>Style Score</p>
+				<span><?php dabc_the_style_rating(); ?> / 100</span>
+				<p>RB Style</p>
+			</div>
+			<div class="large-3 small-3 columns">
+				<span><?php dabc_the_untappd_rating_score(); ?> / 5.0</span>
+				<p>Untappd</p>
 			</div>
 			<div class="large-3 small-3 columns">
 				<span><?php dabc_the_abv(); ?></span>
 				<p>ABV</p>
-			</div>
-			<div class="large-3 small-3 columns">
-				<span><?php dabc_the_calories(); ?></span>
-				<p>Calories</p>
 			</div>
 		</div>
 	</div>
