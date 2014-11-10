0ac2123ea3136f1a925973f251c739abd8bf6931
Jeff Stieler <jeff.m.stieler@gmail.com>
2014-11-09 21:00:35 -0700

    Use store info template part on single store.

--- wp-content/themes/brewtah/single-dabc-store.php
+++ wp-content/themes/brewtah/single-dabc-store.php
@@ -7,27 +7,10 @@
 	<?php while (have_posts()) : the_post(); ?>
 		<article <?php post_class() ?> id="post-<?php the_ID(); ?>">
 			<header>
-				<h1 class="entry-title"><?php the_title(); ?></h1>
-				<span class="phone">
-					<a href="<?php dabc_the_store_tel_link(); ?>"><?php dabc_the_store_phone_number(); ?></a>
-				</span>
-				<span class="address">
-					<?php dabc_the_store_address(); ?>
-				</span>
-
+				<?php get_template_part( 'parts/store-info' ); ?>
 			</header>
 			<?php do_action('brewtah_post_before_entry_content'); ?>
 			<div class="entry-content">
-
-			<?php if ( has_post_thumbnail() ): ?>
-				<div class="row">
-					<div class="column">
-						<?php the_post_thumbnail('', array('class' => 'th')); ?>
-					</div>
-				</div>
-			<?php endif; ?>
-
-			<?php the_content(); ?>
 			<?php get_template_part( 'parts/store-inventory' ); ?>
 			</div>
 			<?php do_action('brewtah_post_before_comments'); ?>
