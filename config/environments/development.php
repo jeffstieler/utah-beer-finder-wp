9af69b8aa607c155039a30d39f9d3f68dd64a015
Jeff Stieler <jeff.m.stieler@gmail.com>
2015-02-02 18:34:24 -0700

    Add API key configuration constants.

--- config/environments/development.php
+++ config/environments/development.php
@@ -8,6 +8,11 @@ define('DB_HOST', getenv('DB_HOST') ? getenv('DB_HOST') : 'localhost');
 define('WP_HOME', getenv('WP_HOME'));
 define('WP_SITEURL', getenv('WP_SITEURL'));
 
+define('GOOGLE_MAPS_API_KEY', getenv('GOOGLE_MAPS_API_KEY'));
+
+define('UNTAPPD_CLIENT_ID', getenv('UNTAPPD_CLIENT_ID'));
+define('UNTAPPD_CLIENT_SECRET', getenv('UNTAPPD_CLIENT_SECRET'));
+
 define('SAVEQUERIES', true);
 define('WP_DEBUG', true);
 define('SCRIPT_DEBUG', true);
--- config/environments/production.php
+++ config/environments/production.php
@@ -8,6 +8,11 @@ define('DB_HOST', getenv('DB_HOST') ? getenv('DB_HOST') : 'localhost');
 define('WP_HOME', getenv('WP_HOME'));
 define('WP_SITEURL', getenv('WP_SITEURL'));
 
+define('GOOGLE_MAPS_API_KEY', getenv('GOOGLE_MAPS_API_KEY'));
+
+define('UNTAPPD_CLIENT_ID', getenv('UNTAPPD_CLIENT_ID'));
+define('UNTAPPD_CLIENT_SECRET', getenv('UNTAPPD_CLIENT_SECRET'));
+
 ini_set('display_errors', 0);
 define('WP_DEBUG_DISPLAY', false);
 define('SCRIPT_DEBUG', false);
